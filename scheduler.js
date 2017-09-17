5ebddf71e5cd83f57d765e1777ffca69be2461fe
fluxflexer <git@ringforum.de>
2017-09-17 18:15:37 +0200

    Working version

--- scheduler.js
+++ scheduler.js
@@ -20,41 +20,45 @@ module.exports.eventEmitter = eventEmitter;
 module.exports.InitScheduler = function() {
 
     var schedule = require('node-schedule');
-    var timerplan = require('./timerplan.json');
+    var timerplan = require('./timerplan.json');        //timerplan laden
 
 //console.log(timerplan.timer[0]);
 
 
-    var timerSchedule = schedule.scheduleJob("* * * * *", function(){
+    // var timerSchedule =  schedule.scheduleJob("* * * * *", function(){
+    //     updateStates(timerplan)
+    // });     //cronjob einrichten: jede minute
+
+    setInterval(function(){
         updateStates(timerplan)
-    });
+    },2000)
 
 
 };
 
 function updateStates(timerplan){
 
-  timerplan.timer.forEach(function (acttimer){
-      var d = new Date();
+  timerplan.timer.forEach(function (acttimer){              //schleife über jeden timer im timerplan
+      var d = new Date();                                   //datum holen
       var actDay=d.getDay();
 
 
-      if (parseInt(actDay)===0) {actDay=7}
+      if (parseInt(actDay)===0) {actDay=7}                  //wenn tag =0 (sonntag) dann tag = 7
 
-      var actMonth=d.getMonth()+1;
+      var actMonth=d.getMonth()+1;                          //januar= 0
       var actHour=d.getHours();
       var actMin=d.getMinutes();
       var checkDay =[];
       var checkMonth =[];
-      var actFloatingtime=actHour+actMin/60;
+      var actFloatingtime=actHour+actMin/60;                //Zeit als dezimalbruch
 
+      acttimer.resultstate = acttimer.defaultState;     //default state
 
-
-  acttimer.schedules.forEach( function(schedule)
+  acttimer.schedules.forEach( function(schedule)           //schleife über alle schedules
       {
 
           if (schedule.month=="*"){
-              checkMonth="1,2,3,4,5,6,7,8,9,10,11,12"
+              checkMonth=[1,2,3,4,5,6,7,8,9,10,11,12]      //wenn * dann alle Monate
           }
           else{
               checkMonth=schedule.month;
@@ -63,7 +67,7 @@ function updateStates(timerplan){
 
 
 
-          if (schedule.dayOfWeek=="*"){
+          if (schedule.dayOfWeek=="*"){                 //wenn * dann alle tage
               checkDay=[1,2,3,4,5,6,7];
           }
           else{
@@ -71,17 +75,16 @@ function updateStates(timerplan){
           }
 
 
-          acttimer.resultstate = acttimer.defaultState;
 
 
-          if (checkDay.indexOf(actDay) >= 0 && checkMonth.indexOf(actMonth) >=0) {
 
-              schedule.time.forEach(function (duration) {
+          if (checkDay.indexOf(actDay) >= 0 && checkMonth.indexOf(actMonth) >=0) {      //prüfen ob tag und monat passen
+
+              schedule.time.forEach(function (duration) {       //schleife über alle zeiträume
 
                   duration = duration.replace(" ", "");
-                  var stateBorders = duration.split("-");
-                  var startTime = stateBorders[0];
-                  var endTime = stateBorders[1];
+                  var startTime = duration.split("-")[0];
+                  var endTime = duration.split("-")[1];
 
                   var stateBegin = getFloatingTime(startTime);
                   var stateEnd = getFloatingTime(endTime);
@@ -101,21 +104,73 @@ function updateStates(timerplan){
           }     //if actDay
 
 
-      }) //loop schedules
+      }); //loop schedules
 
-      console.log(actHour + ":" +actMin +" "+ acttimer.client + " Status= " + acttimer.resultstate);
 
+      setState(acttimer);
 
 });     //lop timer
+
+}
+
+function setState(timer){
+
+    console.log(timer.client + "=" + timer.resultstate)
+switch (timer.protocol){
+    case "ESPEasy":
+        var options= {};
+        options.host=timer.host;
+        options.port=timer.port;
+    if (timer.resultstate===timer.activeState){
+
+
+        options.path=timer.onMessage;
+
+
+    }
+    else{
+        options.path=timer.offMessage;
+
+    }
+        console.log(options);
+        callUrl(options)
+
+    break;
+}
+
+
+
+}
+
+
+function callUrl(options){
+
+    var http = require('http');
+
+
+//options für den get-aufruf aus dem command zusammenstellen
+
+
+
+    // get aufruf
+    http.get(options, function (resp) {
+        resp.on('data', function (chunk) {
+            //do something with chunk
+
+        });
+    }).on("error", function (e) {
+        console.log("Got error: " + e.message);
+    });
+
 
 }
 
 
 function getFloatingTime(timevalue){
 
-    var splitter= timevalue.split(":");
-    var hours= parseInt(splitter[0]);
-    var minutes = parseInt(splitter[1])/60;
+
+    var hours =   parseInt(timevalue.split(":")[0]);
+    var minutes = parseInt(timevalue.split(":")[1])/60;
     return hours + minutes;
 
 
